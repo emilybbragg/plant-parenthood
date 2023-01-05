3de2b1f0c99d6e379da1f7fc18867aeb09bc1a78
Emily <emilybribragg@gmail.com>
2023-01-05 16:01:55 -0500

    Initialized component

---
+++ client/src/components/MyProfilePage.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+function MyProfile() {
+
+
+  return (
+
+    <>
+      <div>welcome to my profile!</div>
+
+    </>
+  );
+}
+
+export default MyProfile;
