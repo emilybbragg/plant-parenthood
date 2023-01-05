793ac505961a9f0334f2a403963dd92b2d0e5d88
Emily <emilybribragg@gmail.com>
2023-01-05 16:01:46 -0500

    Styling and added correct link paths

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -21,15 +21,23 @@ function NavBar({ user, setUser }) {
 
   return (
     <>
-      <div className="">
-        <p className="">Welcome, {user.username}!</p>
-        <div className="">
-          <Link to="/home" className="nav">Home</Link>
-          {/* <Link to="/myreviews" className="nav"> My Reviews </Link> */}
-          {/* <Link to="/myalbums" className="nav"> My Reviewed Albums </Link> */}
-          <button onClick={handleLogoutClick} className="">Logout</button>
+      <div className="bg-white flex items-center justify-center w-full h-[80px]">
+        <span className="text-black font-serif font-semibold text-xl">Good Morning, {user.username}!</span>
+      </div>
+      <span className=""></span>
+
+      <div className="bg-emerald-700 p-3">
+        <div className="flex justify-between">
+
+          <Link to="/posts" className="bg-white w-[100px] h-[50px] p-3 rounded-lg flex justify-center items-center">Home</Link>
+          <Link to="/myprofile" className="bg-white w-[100px] h-[50px] p-3 rounded-lg flex justify-center items-center">My Profile</Link>
+
+          <button onClick={handleLogoutClick} className="bg-white w-[100px] h-[50px] p-3 rounded-lg flex justify-center items-center">Logout</button>
+
         </div>
       </div>
+
+
     </>
   )
 }
